9f9ff1c7356b1cd95fff0b663f55920a655cfb3e
Guille <glopezf@gmail.com>
2026-07-12 09:18:36 -0400

    Trigger workflow with latest changes

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -131,9 +131,10 @@ def add_review(request):
             return JsonResponse({"status":200})
         except:
             print(f"--> Error al procesar la review: {e}")
-            return JsonResponse({"status":401,"message":"Error in posting review"})
+            return JsonResponse({"status": 401,"message": "Error in posting review"})
     else:
-        return JsonResponse({"status":403,"message":"Unauthorized"})
+        return JsonResponse({"status": 403,"message": "Unauthorized"})
+
 
 def get_cars(request):
     count = CarMake.objects.filter().count()
