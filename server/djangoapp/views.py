df7a5b1439bb1e8289f35174b95a107d76587248
Guille <glopezf@gmail.com>
2026-07-12 09:33:48 -0400

    Resolviendo conflictos de sincronización

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,8 +1,6 @@
 # Uncomment the required imports before adding the code
 
-#from django.shortcuts import render
 from django.contrib.auth.models import User
-#from django.contrib.auth import logout as auth_logout
 from django.contrib.auth import login, authenticate
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
